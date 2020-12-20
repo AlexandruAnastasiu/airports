c0ed966f96ad3fba5b773918806c97aa978d9499
Anastasiu Alexandru <alexandru.anastasiu@yahoo.com>
2020-12-20 19:18:13 +0200

    add Worldwide Flight Traffic Statistics on homepage

--- src/main/java/ro/siit/airports/service/impl/FlightServiceImpl.java
+++ src/main/java/ro/siit/airports/service/impl/FlightServiceImpl.java
@@ -42,7 +42,6 @@ public class FlightServiceImpl implements FlightService {
         return flights.stream()
                 .filter(f -> f.getDeparture().toLocalDate().equals(LocalDateTime.now().toLocalDate()))
                 .collect(Collectors.toList());
-
     }
 
     @Override
@@ -53,7 +52,6 @@ public class FlightServiceImpl implements FlightService {
         return flights.stream()
                 .filter(f -> f.getArrival().toLocalDate().equals(LocalDateTime.now().toLocalDate()))
                 .collect(Collectors.toList());
-
     }
 
     @Override
@@ -135,5 +133,14 @@ public class FlightServiceImpl implements FlightService {
         initialFlight.setAirline(airline);
 
         return initialFlight;
+    }
+
+    @Override
+    public Integer getCurrentTraffic() {
+        Integer currentFlights = flightRepository.findAll().stream()
+                .filter(f -> f.getArrival().isAfter(LocalDateTime.now()) && f.getDeparture().isBefore(LocalDateTime.now()))
+                .reduce(0, (a,b) -> a + 1, (a,b) -> a + b);
+
+        return currentFlights;
     }
 }
